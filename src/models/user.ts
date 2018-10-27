7f255fb537efe480963ab5d072bb47e7fb7afe99
moustaxx <maciasm1303@gmail.com>
2018-10-27 20:09:02 +0200

    update User schema

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,27 +1,32 @@
 import mongoose = require('mongoose');
 import passportLocalMongoose = require('passport-local-mongoose');
 
-const UserSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		unique: true,
+const UserSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			unique: true,
+			trim: true,
+		},
+		email: {
+			type: String,
+			unique: true,
+			required: true,
+			trim: true,
+		},
 	},
-	email: {
-		type: String,
-		unique: true,
-		required: true,
-	},
-	createdOn: {
-		type: Date,
-		default: Date.now
-	},
-}) as mongoose.PassportLocalSchema;
+	{
+		collection: 'User',
+		timestamps: true,
+	}
+) as mongoose.PassportLocalSchema;
 
 export interface IUser extends mongoose.PassportLocalDocument {
 	name: string;
 	email: string;
+	updatedOn: Date;
 	createdOn: Date;
 }
-UserSchema.plugin(passportLocalMongoose, {usernameField: 'name'});
+UserSchema.plugin(passportLocalMongoose, { usernameField: 'name' });
 export default mongoose.model<IUser>('User', UserSchema);
